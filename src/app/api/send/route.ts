94a27fe651d0f98f3bb8a297c6f5e86afd1d018f
heltleo <heltleo59@gmail.com>
2025-05-02 20:32:32 +0800

    clean code

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,9 +1,9 @@
 import { EmailTemplate } from "@/components/email-template";
 import { config } from "@/data/config";
-import { Resend } from "resend";
+// import { Resend } from "resend";
 import { z } from "zod";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 const Email = z.object({
   fullName: z.string().min(2, "Full name is invalid!"),
@@ -11,34 +11,34 @@ const Email = z.object({
   message: z.string().min(10, "Message is too short!"),
 });
 export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-    console.log(body);
-    const {
-      success: zodSuccess,
-      data: zodData,
-      error: zodError,
-    } = Email.safeParse(body);
-    if (!zodSuccess)
-      return Response.json({ error: zodError?.message }, { status: 400 });
+  // try {
+  //   const body = await req.json();
+  //   console.log(body);
+  //   const {
+  //     success: zodSuccess,
+  //     data: zodData,
+  //     error: zodError,
+  //   } = Email.safeParse(body);
+  //   if (!zodSuccess)
+  //     return Response.json({ error: zodError?.message }, { status: 400 });
 
-    const { data: resendData, error: resendError } = await resend.emails.send({
-      from: "Porfolio <onboarding@resend.dev>",
-      to: [config.email],
-      subject: "Contact me from portfolio",
-      react: EmailTemplate({
-        fullName: zodData.fullName,
-        email: zodData.email,
-        message: zodData.message,
-      }),
-    });
+  //   const { data: resendData, error: resendError } = await resend.emails.send({
+  //     from: "Porfolio <onboarding@resend.dev>",
+  //     to: [config.email],
+  //     subject: "Contact me from portfolio",
+  //     react: EmailTemplate({
+  //       fullName: zodData.fullName,
+  //       email: zodData.email,
+  //       message: zodData.message,
+  //     }),
+  //   });
 
-    if (resendError) {
-      return Response.json({ resendError }, { status: 500 });
-    }
+  //   if (resendError) {
+  //     return Response.json({ resendError }, { status: 500 });
+  //   }
 
-    return Response.json(resendData);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
+  //   return Response.json(resendData);
+  // } catch (error) {
+  //   return Response.json({ error }, { status: 500 });
+  // }
 }
